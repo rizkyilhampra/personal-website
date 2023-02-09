ce72f62f6d0bdb9ce6ac916dd17a93d82550b2d4
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-02-09 23:24:52 +0800

    v1.2.1

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// function myFunction(x) {
-//     var element = document.getElementById("custom-col-resp")
-//     if (x.matches) { // If media query matches
-//         element.classList.toggle("col-2");
-//     } else {
-//         element.classList.toggle("col-1");
-//         element.classList.remove("col-2");
-//         element2.classList.toggle("col-1");
-//     }
-// }
-// var x = window.matchMedia("(max-width: 568px)")
-// myFunction(x) // Call listener function at run time
-// x.addListener(myFunction) // Attach listener function on state changes
-const navbar = document.getElementsByTagName("nav")[0];
+window.onload = function () {
+    setTimeout(function () {
+        hideLoading();
+    }, 800);
+};
 function hideLoading() {
     document.getElementById('loading').style.opacity = '0';
     document.getElementById('loading').style.zIndex = '-1';
     AOS.init();
 }
 window.addEventListener('scroll', function scrollShadow() {
+    const navbar = document.getElementsByTagName("nav")[0];
     navbar.removeAttribute("data-aos", "data-aos-duration");
     if (window.scrollY > 5) {
         navbar.classList.replace("bg-putih", "bg-blur");
